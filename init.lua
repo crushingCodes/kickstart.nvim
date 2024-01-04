e67895f98fbe1aba26bed7837abf0a4de05fe587
Gavin <26400289+crushingCodes@users.noreply.github.com>
2024-01-04 16:38:11 +0900

    add nvim tree and theme

--- init.lua
+++ init.lua
@@ -189,14 +189,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  },
 
   {
     -- Set lualine as statusline
@@ -205,7 +197,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'ayu-dark',
         component_separators = '|',
         section_separators = '',
       },
@@ -266,8 +258,17 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
+-- nvim-tree
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'Shatur/neovim-ayu',
+    config = function()
+      require('ayu').setup({})
+      vim.cmd.colorscheme 'ayu-dark'
+    end,
+    priority = 1000,
+  },
+  { 'nvim-tree/nvim-tree.lua', config = true },
+  { 'nvim-tree/nvim-web-devicons' },
+}
