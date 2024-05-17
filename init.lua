f9f56e7e41977fb47132ca54db7b9a90ffe4f402
Gavin <26400289+crushingCodes@users.noreply.github.com>
2024-05-17 21:12:19 +0900

    update shortcuts

--- init.lua
+++ init.lua
@@ -947,5 +947,6 @@ vim.api.nvim_create_autocmd('BufRead', {
   end,
 })
 
+vim.treesitter.language.register("markdown", "octo")
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
