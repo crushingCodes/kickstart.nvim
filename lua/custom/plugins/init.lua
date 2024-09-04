3d07fece2cad1bc1055b63374fff72024a727321
Gavin <26400289+crushingCodes@users.noreply.github.com>
2024-09-04 17:57:29 +0900

    config fixes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -717,7 +717,6 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
     config = function()
       require('treesj').setup {--[[ your config ]]
-        notify = false,
       }
     end,
   },
