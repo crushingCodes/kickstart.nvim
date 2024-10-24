86f988ec26bdf4b36a8d8e02e4f3fb916f58b11e
Gavin <26400289+crushingCodes@users.noreply.github.com>
2024-10-24 11:15:00 +0900

    add chatgpt

--- init.lua
+++ init.lua
@@ -1433,7 +1433,9 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+  -- custom plugins
   require 'custom.plugins',
+  require 'custom.plugins.chatgpt',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
@@ -1830,30 +1832,32 @@ vim.api.nvim_set_keymap('n', '<leader>p', ':lua _G.move_to_parent_node()<CR>', {
 local curl = require 'curl'
 curl.setup {}
 
-vim.keymap.set('n', '<leader>cc', function()
-  curl.open_curl_tab()
-end, { desc = 'Open a curl tab scoped to the current working directory' })
+vim.api.nvim_set_keymap('n', '<leader>c', ':ChatGPT<CR>', { noremap = true, silent = true })
 
-vim.keymap.set('n', '<leader>co', function()
-  curl.open_global_tab()
-end, { desc = 'Open a curl tab with gloabl scope' })
+-- vim.keymap.set('n', '<leader>cc', function()
+--   curl.open_curl_tab()
+-- end, { desc = 'Open a curl tab scoped to the current working directory' })
+--
+-- vim.keymap.set('n', '<leader>co', function()
+--   curl.open_global_tab()
+-- end, { desc = 'Open a curl tab with gloabl scope' })
 
--- These commands will prompt you for a name for your collection
-vim.keymap.set('n', '<leader>csc', function()
-  curl.create_scoped_collection()
-end, { desc = 'Create or open a collection with a name from user input' })
-
-vim.keymap.set('n', '<leader>cgc', function()
-  curl.create_global_collection()
-end, { desc = 'Create or open a global collection with a name from user input' })
-
-vim.keymap.set('n', '<leader>fsc', function()
-  curl.pick_scoped_collection()
-end, { desc = 'Choose a scoped collection and open it' })
-
-vim.keymap.set('n', '<leader>fgc', function()
-  curl.pick_global_collection()
-end, { desc = 'Choose a global collection and open it' })
+-- -- These commands will prompt you for a name for your collection
+-- vim.keymap.set('n', '<leader>csc', function()
+--   curl.create_scoped_collection()
+-- end, { desc = 'Create or open a collection with a name from user input' })
+--
+-- vim.keymap.set('n', '<leader>cgc', function()
+--   curl.create_global_collection()
+-- end, { desc = 'Create or open a global collection with a name from user input' })
+--
+-- vim.keymap.set('n', '<leader>fsc', function()
+--   curl.pick_scoped_collection()
+-- end, { desc = 'Choose a scoped collection and open it' })
+--
+-- vim.keymap.set('n', '<leader>fgc', function()
+--   curl.pick_global_collection()
+-- end, { desc = 'Choose a global collection and open it' })
 
 vim.keymap.set('n', '<leader>q', function()
   require('notify').dismiss()
