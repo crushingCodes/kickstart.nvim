ded8fb63040cd5a77cafa1ab489c37e8020adc5a
Gavin <26400289+crushingCodes@users.noreply.github.com>
2024-11-15 08:14:30 +0900

    comment out python plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -843,18 +843,18 @@ return {
       require('ts-error-translator').setup()
     end,
   },
-  {
-    'alexpasmantier/pymple.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'MunifTanjim/nui.nvim',
-      -- optional (nicer ui)
-      'stevearc/dressing.nvim',
-      'nvim-tree/nvim-web-devicons',
-    },
-    build = ':PympleBuild',
-    config = function()
-      require('pymple').setup()
-    end,
-  },
+  -- {
+  --   'alexpasmantier/pymple.nvim',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'MunifTanjim/nui.nvim',
+  --     -- optional (nicer ui)
+  --     'stevearc/dressing.nvim',
+  --     'nvim-tree/nvim-web-devicons',
+  --   },
+  --   build = ':PympleBuild',
+  --   config = function()
+  --     require('pymple').setup()
+  --   end,
+  -- },
 }
